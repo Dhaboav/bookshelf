908037ad5a354c4da7131af68ef3031bbe0e6560
dhaboav <100108392+Dhaboav@users.noreply.github.com>
2025-11-15 22:01:18 +0700

    refactor(FE): Update import paths for consistency in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import BookPage from '@/pages/BookPage';
-import Dashboard from './pages/Dashboard';
-import Docs from './pages/Docs';
-import Landing from './pages/Landing';
-import NotFound from './pages/NotFound';
+import Dashboard from '@/pages/Dashboard';
+import Docs from '@/pages/Docs';
+import Landing from '@/pages/Landing';
+import NotFound from '@/pages/NotFound';
 
 const queryClient = new QueryClient();
 
